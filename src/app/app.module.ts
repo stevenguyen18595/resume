41b4176d6f5aac976a3bc05887fb8f2760553e86
AzureAD\SteveNguyen <steve.nguyen@patientzero.com.au>
2022-09-17 22:46:30 +1000

    update side-pannel design

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,20 +7,37 @@ import { NavBarComponent } from './nav-bar/nav-bar.component';
 import { FooterComponent } from './footer/footer.component';
 import { SidePannelComponent } from './side-pannel/side-pannel.component';
 import { MainPannelComponent } from './main-pannel/main-pannel.component';
-
+import {MatListModule} from '@angular/material/list';
+import {MatIconModule} from '@angular/material/icon';
+import {MatChipsModule} from '@angular/material/chips';
+import { HttpClientModule } from "@angular/common/http";
+import { MatIconRegistry } from "@angular/material/icon";
+import { DomSanitizer } from "@angular/platform-browser";
 @NgModule({
   declarations: [
     AppComponent,
     NavBarComponent,
     FooterComponent,
     SidePannelComponent,
-    MainPannelComponent
+    MainPannelComponent,
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    MatIconModule,
+    MatListModule,
+    MatChipsModule,
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {
+  constructor(private matIconRegistry:MatIconRegistry, private domSanitizer : DomSanitizer){
+    this.matIconRegistry.addSvgIcon(
+      'leet-code',
+      this.domSanitizer.bypassSecurityTrustResourceUrl("../../assets/img/leetcode-white.svg")
+    );
+
+  }
+ }
